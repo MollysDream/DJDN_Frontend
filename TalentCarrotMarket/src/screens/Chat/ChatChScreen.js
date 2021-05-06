c5439c64aa501257083a80836a8ec41ce28f4c74
Kim234 <djehoaos@naver.com>
2021-05-06 20:54:42 +0900

    socket

--- TalentCarrotMarket/src/screens/Chat/ChatChScreen.js
+++ TalentCarrotMarket/src/screens/Chat/ChatChScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import {
     View,
     Text,
@@ -6,15 +6,48 @@ import {
     FlatList,
     TouchableOpacity
 } from 'react-native';
-import {List, Divider} from 'react-native-paper'
+import { List, Divider } from 'react-native-paper';
+import firestore from '@react-native-firebase/firestore';
 
 const ChatChScreen =({navigation})=>{
+
+      const [threads, setThreads] = useState([]);
+
+      useEffect(() => {
+        const unsubscribe = firestore()
+        .collection('Trade')
+        .onSnapshot(querySnapshot => {
+          const threads = querySnapshot.docs.map(documentSnapshot =>{
+            return {
+              _id: documentSnapshot.id,
+              name:'',
+              ...documentSnapshot.data()
+            };
+        });
+
+        setThreads(threads);
+        });
+
+        return () => unsubscribe();
+      }, []);
     
-        return (
-            <View style={styles.container}>
-                <ChatListItem chatRoom />
-            </View>
-        );
+      return (
+          <View style={styles.container}>
+            <FlatList 
+              data={threads}
+              keyExtractor={item=>item._id}
+              ItemSeparatorComponent={() => <Divider />}
+              renderItem={({}) => (
+                <List.Item
+                  description="Item description"
+                  titleNumberOfLines={1}
+                  descriptionStyle={styles.listDescription}
+                  descriptionNumberOfLines={1}
+                  />
+              )}
+            />
+          </View>
+      );
     
 }
 
@@ -22,9 +55,6 @@ const styles = StyleSheet.create({
     container: {
       backgroundColor: '#f5f5f5',
       flex: 1
-    },
-    listTitle: {
-      fontSize: 22
     },
     listDescription: {
       fontSize: 16
