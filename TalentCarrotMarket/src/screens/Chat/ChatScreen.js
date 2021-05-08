470bff5d714215228680df4c5831228ce40de5d2
JUNYEOP <junyeoop@gmail.com>
2021-05-08 18:35:57 +0900

    50% chat

--- TalentCarrotMarket/src/screens/Chat/ChatScreen.js
+++ TalentCarrotMarket/src/screens/Chat/ChatScreen.js
@@ -13,27 +13,58 @@ import {GiftedChat} from 'react-native-gifted-chat'
 import io from "socket.io-client";
 import AsyncStorage from '@react-native-community/async-storage';
 
-function ChatScreen(props) {
-    const [message, setMessage] = useState("");
-    const [messages, setMessages] = useState([]);
-    var [userId, setUserid] = useState("");
-    var socket = io("http://10.0.2.2:3002");
+let socket;
+let messages
 
+function ChatScreen(props) {
+    // const [message, setMessage] = useState("");
+    let [messages, setMessages] = useState([]);
+    let [userId, setUserid] = useState("");
+
+    let socketId;
     //gift chat 관련
     useEffect(() => {
       AsyncStorage.getItem('user_id')
     .then((value) => {
       setUserid(value);
-      console.log('name is ', value);
-      console.log(userId);
+      // console.log('name is ', value);
+      // console.log(userId);
     });
 
-      // socket.emit('connect', 1);
+      // 소켓 연결
+      socket = io("http://10.0.2.2:3002");
 
+      // 여기서 room1이 하드코딩
+      socket.emit('joinRoom','room1');
+      console.log('room1에 접속');
+
+      // 내 현재 소켓 아이디
+      console.log(socket.id);
+
+      socketId = socket.id;
+
+      // if(messages !== null){
+      //   let helpArray = [];
+      //   let text = {
+      //     _id: 1,
+      //     text: 'Hello developer',
+      //     createdAt: new Date(),
+      //     user: {
+      //       _id: 2,
+      //       name: 'React Native',
+      //       avatar: 'https://placeimg.com/140/140/any',
+      //     }
+      //   };
+      //   helpArray.push(text);
+      //
+      //   setMessages(GiftedChat.append(helpArray));
+      // }
+
+      // 처음 시작 메세지, messages가 비어있으니까 setState로 설정해
       setMessages([
         {
           _id: 1,
-          text: 'Hello developer',
+          text: userId+'님께서 채팅을 주셨어요.',
           createdAt: new Date(),
           user: {
             _id: 2,
@@ -42,47 +73,72 @@ function ChatScreen(props) {
           },
         },
       ])
-      console.log("messages 출력~ "+ messages[0]);
+      // console.log("messages 출력 "+ messages[0]);
 
+
+
+
+      console.log("Hello developer _id 출력 : " );
+
+      // 서버에 있는 채팅 불러오기...? 라고 이해하면 될듯?
+        socket.on('chat message to client', (msg) => {
+        let newMessage= msg;
+        newMessage[0].user._id = 1;
+        setMessages( (previous) => GiftedChat.append(previous, newMessage));
+        console.log("UseEffect 안에 socket.on임// 현재 사용중인 소켓 아이디 : ",socket.id);
+        console.log("옛날 message 받아와서 출력할 때 새로 append 되는 msg : " + msg);
+
+        console.log("message 길이 : " + messages.length);
+        console.log("client에서 chat message 받는거 :" + msg[0].text);
+
+
+
+        // onSendDB(newMessage);
+      });
+
+
+
+        // 채팅방 떠나기
       return () => {
-        // socket.emit('disconnect', 1);
+        socket.emit('leaveRoom','room1');
+        // socket.emit('disconnect', []);
         socket.disconnect();
       };
 
     },[]);
 
-    socket.on('newMessage', (newMessage) => {
-      let newMessaged = newMessage;
-      // newMessaged[0].user._id = 1;
-      setMessages((previous) => GiftedChat.append(previous, newMessaged));
-      console.log("client에서 chat message 받는거 ");
-      // console.log(newMessage);
-      // onSendDB(newMessage);
-    });
 
+
+
+    let count=0;
+
+    // 내가 send버튼 눌렀을때 발생하는 이벤트..?
     function onSend(newMessages = []){
-      setMessages(GiftedChat.append(messages, newMessages))
-      socket.emit("chat message", newMessages)
-      console.log("client에서 지금 메세지 보내는 중");
-      console.log(newMessages);
+
+      if(0){
+
+      }
+
+      else{
+      socket.emit("chat message to server", newMessages);
+      console.log('onSend가 호출됨!! count : ' + count++);
+      setMessages((prevMessages)=>GiftedChat.append(prevMessages, newMessages));
+
+      console.log("onSend에서 쓰는 중 // 현재 사용중인 소켓 아이디 : ",socket.id);
+
+
+
+      console.log("client에서 지금 메세지 보내는 중: " + newMessages[0]._id);
+      console.log(messages.length);
 
       //이게 실제 보여지는 텍스트임
       console.log(newMessages[0].text);
+      }
     };
 
 
 
-    // const chatMessages=messages.map(chatMessage=>
-    //   <Text style={{borderWidth:2, top:500}}>{chatMessage}</Text>
-    // )
-
-    const submitChatMessage = () =>{
-      console.log("hi"+socket.connect().connected);
-      socket.emit('chat message', message);
-      setMessage('')
-    }
-
-
+    // user 정보 좀 손봐야할듯
     return (
     <GiftedChat
       messages={messages}
