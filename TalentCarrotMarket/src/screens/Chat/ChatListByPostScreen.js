e384d8d3a55b16a790c8029e1fce05ade80e540a
teller2016 <teller2016@ajou.ac.kr>
2021-05-18 00:38:08 +0900

    게시물 별 채팅방 확인 스타일링

--- TalentCarrotMarket/src/screens/Chat/ChatListByPostScreen.js
+++ TalentCarrotMarket/src/screens/Chat/ChatListByPostScreen.js
@@ -14,6 +14,8 @@ import { List, Divider } from 'react-native-paper';
 import firestore from '@react-native-firebase/firestore';
 import {AnimatedAbsoluteButton} from 'react-native-animated-absolute-buttons';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import AsyncStorage from '@react-native-community/async-storage';
 import requestUser from "../../requestUserAPI";
 import request from '../../requestAPI';
@@ -23,6 +25,7 @@ import {
 	widthPercentageToDP as wp,
 	heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
+import requestChatAPI from "../../requestChatAPI";
 
 
 let userData;
@@ -60,20 +63,23 @@ function ChatListByPostScreen(props) {
 			console.log(`지금 랜더링 ${count++}: 번 실행됐다!`);
 			console.log(roomInfo);
 			roomInfo.map(async (data)=>{
+				let latestChat = await requestChatAPI.getLatestChat(data._id);
+				//console.log(latestChat);
 				let partnerUserData;
-				let myUserData;
+				//let myUserData;
 				if(currentId == data.hostId){
-					myUserData = await requestUser.getUserData(data.hostId);
+					//myUserData = await requestUser.getUserData(data.hostId);
 					partnerUserData = await requestUser.getUserData(data.postOwnerId);
 				}else{
 					partnerUserData = await requestUser.getUserData(data.hostId);
-					myUserData = await requestUser.getUserData(data.postOwnerId);
+					//myUserData = await requestUser.getUserData(data.postOwnerId);
 				}
 
 				let postData = await request.getPostTitle(data.postId);
 
 
-				nick = nick.concat({_id : data._id, myUserData : myUserData , partnerUserData : partnerUserData, postData : postData[0] });
+
+				nick = nick.concat({_id : data._id/*, myUserData : myUserData*/ , partnerUserData : partnerUserData, postData : postData[0], latestChat:latestChat });
 				await setNickInfo(nick);
 			})
 		}
@@ -89,19 +95,21 @@ function ChatListByPostScreen(props) {
 			setRoomById(roomInfo);
 
 			roomInfo.map(async (data)=>{
+				let latestChat = await requestChatAPI.getLatestChat(data._id);
+
 				let partnerUserData;
-				let myUserData;
+				//let myUserData;
 				if(currentId == data.hostId){
-					myUserData = await requestUser.getUserData(data.hostId);
+					//myUserData = await requestUser.getUserData(data.hostId);
 					partnerUserData = await requestUser.getUserData(data.postOwnerId);
 				}else{
 					partnerUserData = await requestUser.getUserData(data.hostId);
-					myUserData = await requestUser.getUserData(data.postOwnerId);
+					//myUserData = await requestUser.getUserData(data.postOwnerId);
 				}
 
 				let postData = await request.getPostTitle(data.postId);
 
-				nick = nick.concat({_id : data._id, myUserData : myUserData , partnerUserData : partnerUserData, postData : postData[0] });
+				nick = nick.concat({_id : data._id/*, myUserData : myUserData */, partnerUserData : partnerUserData, postData : postData[0], latestChat:latestChat });
 				setNickInfo(nick);
 
 			})
@@ -116,24 +124,32 @@ function ChatListByPostScreen(props) {
 	}
 
 	function returnFlatListItem(item,index){
-		let myUserData = item.myUserData;
+		//let myUserData = item.myUserData;
 		let partnerUserData = item.partnerUserData;
 		let postData = item.postData;
+		let chat = '';
+		let chatTime = '';
+		if(item.latestChat != null){
+			chat = item.latestChat.text;
+			chatTime = item.latestChat.createdAt;
+		}
 		return(
 			<TouchableHighlight onPress={() => props.navigation.navigate('게시글별 채팅리스트 채팅방', {postOwner: userData, roomInfo: item})}>
 				<View style={styles.chatRoomBox}>
-					<Image style={styles.post_image} source={{ uri: postData.image[0]}} />
-					<View style={{flexDirection:'column'}}>
-						<Text style={styles.postTitle}>{postData.title}</Text>
-						<View style={styles.userDataBox}>
-							<Image style={styles.user_image} source={{uri:partnerUserData.profileImage}}/>
-							<View style={styles.user_data_text}>
-								<Text style={{fontSize:11, color:'grey'}}>{postData.addressName}</Text>
-								<Text style={{fontWeight:'bold',fontSize:15}}>{partnerUserData.nickname}</Text>
-							</View>
 
+					<View style={styles.userDataBox}>
+						<Image style={styles.user_image} source={{uri:partnerUserData.profileImage}}/>
+						<View style={styles.user_data_text}>
+							<Text style={{fontSize:12, color:'grey'}}>{postData.addressName}</Text>
+							<Text style={{fontWeight:'bold',fontSize:20}}>{partnerUserData.nickname}</Text>
+							<View style={{flexDirection:'row'}}>
+								<Icon style={styles.chat_text} name="sms" size={20}/>
+								<Text style={styles.chat_text}>{` ${chat}`}</Text>
+							</View>
 						</View>
+
 					</View>
+					<Text style={styles.chatTime_text}>{chatTime}</Text>
 
 				</View>
 			</TouchableHighlight>
@@ -174,26 +190,16 @@ const styles = StyleSheet.create({
 		flexDirection:'row'
 
 	},
-	postTitle:{
-		fontSize:15,
-		paddingTop:7
-	},
 	userDataBox:{
 		flexDirection:'row',
-		paddingTop: 5
+		paddingTop: 5,
+		paddingLeft: 5,
+		paddingBottom: 5
 	},
-	post_image:{
+	user_image:{
 		width: wp(20),
 		overflow:"hidden",
 		height: hp(20),
-		aspectRatio: 1,
-		borderRadius: 9,
-		marginRight:12
-	},
-	user_image:{
-		width: wp(10),
-		overflow:"hidden",
-		height: hp(10),
 		aspectRatio: 1,
 		borderRadius: 40,
 		marginRight:12,
@@ -202,15 +208,19 @@ const styles = StyleSheet.create({
 	user_data_text:{
 		marginTop: 2
 	},
-	post:{
-		flexDirection: "row",
-		//borderRadius: 15,
-		backgroundColor: "white",
-		borderBottomColor: "#a6e5ff",
-		borderBottomWidth: 1,
-		padding: 10,
-		height: 100
+	chat_text:{
+		//borderWidth: 1,
+		paddingTop: 8,
+		color:'grey',
 	},
+	chatTime_text:{
+		position:'absolute',
+		right: 14,
+		top: 10,
+		color:'grey',
+		fontSize: 15
+	}
+
 });
 
 export default ChatListByPostScreen;
