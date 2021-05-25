7762199d04c4cfcf39e4190a985d3c42c1fd2250
teller2016 <teller2016@ajou.ac.kr>
2021-05-25 16:24:07 +0900

    신고별 화면 분할

--- TalentCarrotMarket/src/adminScreens/Report/ReportScreen.js
+++ TalentCarrotMarket/src/adminScreens/Report/ReportScreen.js
@@ -20,16 +20,45 @@ import {useIsFocused} from "@react-navigation/native";
 
 const ReportScreen = ({navigation}) => {
 
+    const [tab, setTab] = useState(0) // 0 -> 전체신고, 1 -> 게시물 신고, 2 -> 사용자 신고
+
     //인증한 동네 확인 - 사용자
     useEffect(() => {
 
     }, []);
 
+    let Screen = null;
+    if(tab==0)
+        Screen = <Text>전체신고</Text>
+    else if(tab==1)
+        Screen = <Text>게시물 신고</Text>
+    else
+        Screen = <Text>사용자 신고</Text>
 
     return (
         <View style={styles.container}>
 
-            <Text>어드민 화면</Text>
+            <View style={styles.title}>
+                <TouchableOpacity style={{width:'33%', alignItems:'center'}} onPress={()=>setTab(0)}>
+                    <Text style={tab == 0 ? styles.text_on : styles.text_off}>
+                        {`전체 신고`}
+                    </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={{width: '33%', alignItems:'center'}} onPress={()=>setTab(1)}>
+                    <Text style={tab == 1 ? styles.text_on : styles.text_off}>
+                        {`게시글 신고`}
+                    </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={{width: '33%', alignItems:'center'}} onPress={()=>setTab(2)}>
+                    <Text style={tab == 2 ? styles.text_on : styles.text_off}>
+                        {`사용자 신고`}
+                    </Text>
+                </TouchableOpacity>
+            </View>
+
+            <View>{Screen}</View>
 
 
         </View>
@@ -40,12 +69,26 @@ const ReportScreen = ({navigation}) => {
 
 
 const styles = StyleSheet.create({
+    text_on:{
+    fontSize:18,
+        fontWeight: 'bold',
+        color: '#ff5900'
+},
+text_off:{
+    fontSize:18,
+        fontWeight: 'bold',
+        color: 'grey'
+},
+    title:{
+        margin:10,
+        flexDirection:'row'
+    },
     container: {
         flex: 1, //전체의 공간을 차지한다는 의미
         flexDirection: 'column',
         backgroundColor: 'white',
-        paddingLeft: wp(7),
-        paddingRight: wp(7),
+        paddingLeft: wp(2),
+        paddingRight: wp(2),
     },
 
 });
